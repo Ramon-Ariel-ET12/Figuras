85f156cf5645a0fca0aa9d2b3abefad321a31053
Your Name <you@example.com>
2024-08-19 18:16:12 -0300

    Tarea para Grispino

--- Biblioteca/Triangulo.cs
+++ Biblioteca/Triangulo.cs
@@ -7,24 +7,40 @@ public class Triangulo : Figura
 	private double Base;
 	private double Altura;
 	public Triangulo() {}
-	public Triangulo(double lado1, double lado2, double Base, double altura)
+	public Triangulo(double lado1, double lado2, double Base)
 	{
 		Lado1 = lado1;
 		Lado2 = lado2;
 		this.Base = Base;
-		Altura = altura;
-		CalcularArea();
+		CalcularAltura();
 		CalcularPerimetro();
+		CalcularArea();
 	}
 
+	public void CalcularAltura()
+	{
+		double hipotenusa;
+			if (Lado1 >= Lado2 && Lado1 >= Base)
+            {
+                hipotenusa = Lado1;
+            }
+            else if (Lado2 >= Lado1 && Lado2 >= Base)
+            {
+                hipotenusa = Lado2;
+            }
+            else
+            {
+                hipotenusa = Base;
+            }
+		Altura = Math.Sqrt(Math.Pow(hipotenusa, 2) - Math.Pow(Base / 2, 2));
+	}
 	public void Forma()
 	{
-		double alturaTeorica = Math.Sqrt(Math.Pow(Lado1, 2) - Math.Pow(Base / 2, 2));
 		if(Lado1 == Lado2 && Lado1 == Base && Lado2 == Base)
 		{
 			Console.WriteLine("-------- Triangulo Equilatero --------");
 		}
-		else if(Lado1 == Lado2 && Math.Abs(Altura - alturaTeorica) <= 0)
+		else if(Lado1 == Lado2)
 		{
 			Console.WriteLine("-------- Triangulo Isóceles --------");
 		}
@@ -32,7 +48,6 @@ public class Triangulo : Figura
 		{
 			Console.WriteLine("-------- Triangulo Escaleno --------");
 		}
-	
 	}
 
 	public override void CalcularArea()
--- Consola/Program.cs
+++ Consola/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Biblioteca;
-Console.Clear();
 
 Rectangulo rectangulo = new Rectangulo(100, 100);
 
@@ -9,9 +8,9 @@ Circulo circulo= new Circulo(100);
 
 circulo.Mostrar();
 
-Triangulo equilatero = new (20, 20, 20, 20);
-Triangulo isoceles = new (5, 5, 6, 4);
-Triangulo escaleno = new (9, 20, 21, 20);
+Triangulo equilatero = new (20, 20, 20);
+Triangulo isoceles = new (5, 5, 6);
+Triangulo escaleno = new (9, 20, 21);
 
 equilatero.Mostrar();
 isoceles.Mostrar();
